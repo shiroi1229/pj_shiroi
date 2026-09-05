5d98c8df4314e936efb19288a9611f2f66d0953d
Cipas <shiroi1229@gmail.com>
2026-09-05 23:20:13 +0900

    Track requested and actual temporal paths in benchmarks

--- ios/ShiroiVideoForge/Sources/GenerationMetrics.swift
+++ ios/ShiroiVideoForge/Sources/GenerationMetrics.swift
@@ -12,7 +12,8 @@ struct GenerationMetrics: Sendable {
     let outputFrames: Int
     let fps: Int
     let quality: GenerationQuality
-    let temporalMode: TemporalMode
+    let requestedTemporalMode: TemporalMode
+    let actualTemporalPath: TemporalExecutionPath
     let memoryClass: DeviceCapabilities.MemoryClass
     let thermalBefore: String
     let thermalAfter: String
